953354238209faa5a1744a3240c418ffcf0b7fa5
Thorhauer, Simon <simon.thorhauer@getec-net.de>
2022-09-07 15:38:46 +0200

    Display Name Attribute für SnowboarderViewModel. Umstellung Label auf asp-for

--- Models/SnowboarderViewModel.cs
+++ Models/SnowboarderViewModel.cs
@@ -1,12 +1,20 @@
-﻿namespace Avalanche.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Avalanche.Models
 {
     public class SnowboarderViewModel
     {
+        [Display(Name ="Nachname")]
         public string Nachname { get; set; }
+        [Display(Name = "Vorname")]
         public string Vorname { get; set; }
+        [Display(Name = "Künstlername")]
         public string Kuenstlername { get; set; }
+        [Display(Name = "Geburtstag")]
         public DateTime Geburtstag { get; set; }
+        [Display(Name = "Haus Berg")]
         public BergViewModel HausBerg { get; set; }
+        [Display(Name = "Mitgliedsnummer")]
         public string Mitgliednummer { get; set; }
     }
 }
