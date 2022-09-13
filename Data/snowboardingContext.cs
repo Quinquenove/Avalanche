a01ce19f2a5d2d2fb0aa9e4b23f20e25c3e4bd83
Can C <divergenz@outlook.com>
2022-09-13 14:27:47 +0200

    relative path

--- Data/snowboardingContext.cs
+++ Data/snowboardingContext.cs
@@ -31,8 +31,9 @@ namespace Avalanche.Data
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlite("DataSource=C:\\Users\\thorhauer\\Documents\\Schulprojekt\\Avalanche\\snowboarding.db");
+                string sCurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+                string aPath = System.IO.Path.Combine(sCurrentDirectory, "snowboarding.db");
+                optionsBuilder.UseSqlite("DataSource="+ aPath);
             }
         }
 
