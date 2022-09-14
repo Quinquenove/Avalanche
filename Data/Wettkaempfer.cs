05e2c06ae906488f5a2329898d264031474e2385
Vladimir Strelcenko <fiae20j.strelcenko@mm-bbs.de>
2022-09-14 16:11:32 +0200

    aaa

---
+++ Data/Wettkaempfer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Avalanche.Data
+{
+    public partial class Wettkaempfer
+    {
+        public string Snowboarder { get; set; } = null!;
+        public int WettkampfId { get; set; }
+    }
+}
