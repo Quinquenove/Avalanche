a72dea8bc38bbebc316e1baedda7da2c667c08cb
Can C <divergenz@outlook.com>
2022-09-15 13:46:53 +0200

    Wettkampf hinzugefügt

--- Controllers/WettkaempfeController.cs
+++ Controllers/WettkaempfeController.cs
@@ -23,7 +23,6 @@ namespace Avalanche.Controllers
                         Name = wettkampf.Name,
                         Jahr = wettkampf.Jahr,
                         Berg = wettkampf.Berg,
-                        WettkampfId = wettkampf.WettkampfId
                     });
                 }
             }
@@ -43,119 +42,26 @@ namespace Avalanche.Controllers
                     BergList.Add(new SelectListItem() { Value = item.Name, Text = item.Name });
                 }
             }
-            return View(new WettkampfViewModel() { BergList = BergList, Geburtstag= DateTime.Now });
+            return View(new WettkampfViewModel() { BergList = BergList });
         }
 
         [HttpPost]
-        public IActionResult Add(WettkampfViewModel wettkampf)
+        public IActionResult Add(WettkampfViewModel snowboarder)
         {
             using (var Context = new snowboardingContext())
             {
-                Wettkampf wettkampfDB = new Wettkampf()
+                Wettkampf WettkampfDB = new Wettkampf()
                 {
-                    Nachname = wettkampf.Nachname,
-                    Vorname = wettkampf.Vorname,
-                    Kuenstlername = wettkampf.Kuenstlername,
-                    Geburtstag = wettkampf.Geburtstag,
-                    HausBerg = wettkampf.HausBerg,
-                    Mitgliedsnummer = wettkampf.Mitgliedsnummer
+                    Name = snowboarder.Name,
+                    Sponsor = snowboarder.Sponsor,
+                    Jahr = snowboarder.Jahr,
+                    Berg = snowboarder.Berg,
                 };
 
-                Context.Add(wettkampfDB);
+                Context.Add(WettkampfDB);
                 Context.SaveChanges();
             }
             return RedirectToAction(actionName: "Index");
-        }
-
-        [HttpGet]
-        public IActionResult Detail(string wettkampfID)
-        {
-            WettkampfDetailViewModel wettkampfDetail = new WettkampfDetailViewModel();
-            WettkampfViewModel wettkampf;
-            BergViewModel berg;
-            using(var Context = new snowboardingContext())
-            {
-                var wettkampfDB = Context.Wettkaempfe
-                                    .Include(x => x.HausBergNavigation)
-                                    .First(x => x.Mitgliedsnummer.Equals(wettkampfID));
-                wettkampf = new WettkampfViewModel()
-                {
-                    Nachname = wettkampfDB.Nachname,
-                    Vorname = wettkampfDB.Vorname,
-                    Kuenstlername = wettkampfDB.Kuenstlername,
-                    Geburtstag = wettkampfDB.Geburtstag,
-                    HausBerg = wettkampfDB.HausBerg,
-                    Mitgliedsnummer = wettkampfDB.Mitgliedsnummer
-                };
-                berg = new BergViewModel()
-                {
-                    Gebirge = wettkampfDB.HausBergNavigation.Gebirge,
-                    Name = wettkampfDB.HausBergNavigation.Name,
-                    Schwierigkeit = wettkampfDB.HausBergNavigation.Schwierigkeit
-                };
-                var sponsor = new SponsoringViewModel()
-                {
-                    Wettkampf = wettkampf.Mitgliedsnummer,
-                    Sponsor = "TestSponsor",
-                    Vertragsart = "TestVertrag"
-                };
-
-                wettkampfDetail.Wettkampf = wettkampf;
-                wettkampfDetail.Berg = berg;
-                wettkampfDetail.Sponsoring = new List<SponsoringViewModel>() { sponsor };
-
-            }
-            return View(wettkampfDetail);
-        }
-
-        [HttpGet]
-        public IActionResult Update(string wettkampfID)
-        {
-            WettkampfViewModel wettkampf;
-            Wettkampf wettkampfDB;
-            List<SelectListItem> BergList = new List<SelectListItem>();
-            using (var Context = new snowboardingContext())
-            {
-                wettkampfDB = Context.Wettkaempfe.Include(x => x.HausBergNavigation).First(x => x.Mitgliedsnummer.Equals(wettkampfID));
-
-                var bergDataList = Context.Bergs.ToList();
-
-                foreach (var item in bergDataList)
-                {
-                    BergList.Add(new SelectListItem() { Value = item.Name, Text = item.Name });
-                }
-            }
-
-            wettkampf = new WettkampfViewModel()
-            {
-                Nachname = wettkampfDB.Nachname,
-                Vorname = wettkampfDB.Vorname,
-                Kuenstlername = wettkampfDB.Kuenstlername,
-                Geburtstag = wettkampfDB.Geburtstag,
-                Mitgliedsnummer = wettkampfDB.Mitgliedsnummer,
-                HausBerg = wettkampfDB.HausBerg,
-                BergList = BergList
-            };
-
-            return View(wettkampf);
-        }
-
-        [HttpPost]
-        public IActionResult Update(WettkampfViewModel wettkampf)
-        {
-            Wettkampf wettkampfDB;
-            using (var Context = new snowboardingContext())
-            {
-                wettkampfDB = Context.Wettkaempfe.First(x => x.WettkampfID.Equals(wettkampf.WettkampfID));
-
-                wettkampfDB.Name = wettkampf.Name;
-                wettkampfDB.Jahr = wettkampf.Jahr;
-                wettkampfDB.Berg = wettkampf.Berg;
-
-                Context.SaveChanges();
-            }
-
-            return RedirectToAction("Index");
         }
     }
 }
--- Models/WettkampfViewModel.cs
+++ Models/WettkampfViewModel.cs
@@ -1,10 +1,14 @@
-﻿namespace Avalanche.Models
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Avalanche.Models
 {
     public class WettkampfViewModel
     {
         public string Name { get; set; } = null!;
         public byte[] Jahr { get; set; } = null!;
         public string? Berg { get; set; }
-        public int WettkampfId { get; set; }
+        public string? Sponsor { get; set; }
+        public List<SelectListItem> BergList { get; set; }
+
     }
 }
