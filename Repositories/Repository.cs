28008c9a46c3e6250112423e02d5f7afe2726439
Thorhauer, Simon <simon.thorhauer@getec-net.de>
2022-09-27 16:30:55 +0200

    Basisklassen für Repository Pattern mit UnitOfWork implementiert

---
+++ Repositories/Repository.cs
@@ -0,0 +1,50 @@
+﻿using Avalanche.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Avalanche.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly DbSet<T> _entities;
+
+        public Repository(DbContext Context)
+        {
+            _entities = Context.Set<T>();
+        }
+        public void Add(T entity)
+        {
+            _entities.Add(entity);
+        }
+
+        public void AddRange(IEnumerable<T> entities)
+        {
+            _entities.AddRange(entities);
+        }
+
+        public IEnumerable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            return _entities.Where(predicate);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _entities.ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _entities.Find(id);
+        }
+
+        public void Remove(T entity)
+        {
+            _entities.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _entities.RemoveRange(entities);
+        }
+    }
+}
---
+++ Repositories/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using Avalanche.Data;
+
+namespace Avalanche.Repositories
+{
+    public class UnitOfWork : IDisposable
+    {
+        private readonly snowboardingContext _context;
+
+        public UnitOfWork(snowboardingContext context)
+        {
+            _context = context;
+        }
+
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
