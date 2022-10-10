bbf0099c82c499bdd1c68fa24d2fc7135d57be01
Thorhauer, Simon <simon.thorhauer@getec-net.de>
2022-10-10 10:31:46 +0200

    SQL Statements in Ordner gepackt. Nicht benötigte Elemente entfernt

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace Avalanche.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
